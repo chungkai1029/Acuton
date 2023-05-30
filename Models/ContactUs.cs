f41d648f0dafd0ccbb519cb53b49216e850a8650
chungkai1029 <chungkai1029@gmail.com>
2023-05-30 23:36:36 +0800

    Add a template API to recieve form data

---
+++ Models/ContactUs.cs
@@ -0,0 +1,11 @@
+﻿namespace Acuton.Models
+{
+    public class ContactUs
+    {
+        internal string company { get; set; }
+        internal string name { get; set; }
+        internal string email { get; set; }
+        internal string phone { get; set; }
+        internal string content { get; set; }
+    }
+}
