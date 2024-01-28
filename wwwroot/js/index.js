3ef5643ec1558009620491c4cd485ddc409a4991
chungkai1029 <chungkai1029@gmail.com>
2024-01-28 10:44:48 +0800

    Add the onclick method to clear user input

---
+++ wwwroot/js/index.js
@@ -0,0 +1,6 @@
+function clearForm() {
+    console.log("Start to clear the form of HTML.");
+    document.getElementById("form_of_homepage").reset();
+    document.getElementById("form_of_contacts").reset();
+    console.log("Clear the form of HTML finish.");
+}
